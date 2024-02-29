2ef488faa5cda83e6acf488d24b72370e07aaea9
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-02-29 06:12:45 +0100

    Update to 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hello World!";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nosys  .url = "github:divnix/nosys";
   };
 
